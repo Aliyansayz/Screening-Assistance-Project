0cf1ee85fd3b79162a579a988c64c7fbb13e405d
Aliyan Anwar <56589240+Aliyansayz@users.noreply.github.com>
2023-11-03 23:34:37 +0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -27,8 +27,9 @@ def get_pdf_text(filename):
 def create_docs(user_pdf_list, unique_id):
   docs = []
   for filename in user_pdf_list:
-      docs.append(Document( page_content= get_pdf_text(filename), metadata={"name": f"{filename}" , "unique_id":unique_id } ) )
-
+      # docs.append(Document( page_content= get_pdf_text(filename), metadata={"name": f"{filename}" , "unique_id":unique_id } ) )
+      docs.append(pdf_textual(filename))
+      
   return docs
 
 
